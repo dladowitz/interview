a9d1f9cb3dc046b213d81f9bb5aec165e2ee368b
David Ladowitz <david@ladowitz.com>
2016-06-06 17:51:08 -0700

    adding api call to omdb

--- Gemfile
+++ Gemfile
@@ -1,3 +1,10 @@
 source 'https://rubygems.org'
 
+
+# For making http call to the OMDB API
+gem 'httparty'
+
+# For stopping code and debugging
+gem 'pry'
+
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
+require 'httparty'
+require 'pry'
 require 'sinatra'
+
 
 # Changing to use HAML, since I know that better than ERB
 set :haml, :format => :html5
 
 get '/' do
-  haml :index
-  # File.read('/index.html')
 
+  search_query = params[:title]
+
+  omdb_response = HTTParty.get("http://www.omdbapi.com/?s=#{search_query}")
+
+  omdb_hash_response = JSON.parse(omdb_response.body)
+  @movies_array = omdb_hash_response["Search"]
+  # binding.pry
+
+  haml :index
 end
 
 get 'favorites' do
