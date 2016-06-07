14a22253dcf794e832a5cc658b4fcada5f5cabc8
David Ladowitz <david@ladowitz.com>
2016-06-07 11:43:21 -0700

    moving add to favorites to use ajax

--- public/movies.js
+++ public/movies.js
@@ -13,3 +13,16 @@ function toggleDetails(title) {
     document.getElementById(title).style.display = "block";
   }
 }
+
+function addToFavorites(params) {
+  console.log(params)
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      // document.getElementById("demo").innerHTML = xhttp.responseText;
+      console.log(xhttp.responseText);
+    }
+  };
+  xhttp.open("GET", "/favorites?" + params, true);
+  xhttp.send();
+}
